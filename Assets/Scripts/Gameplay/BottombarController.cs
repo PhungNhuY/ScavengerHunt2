5ee61fde39a03ce9d75b82c8ef819b2b2ab2b581
PhungNhuY <yphung282@gmail.com>
2022-11-09 16:24:14 +0700

    09112022

---
+++ Assets/Scripts/Gameplay/BottombarController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottombarController : MonoBehaviour
+{
+    bool status;
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        status = true;
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeStatus()
+    {
+        if (status)
+        {
+            gameObject.transform.position -= new Vector3(0, (cam.orthographicSize * 1.65f * 250f) / 1920f, 0);
+        }
+        else
+        {
+            gameObject.transform.position += new Vector3(0, (cam.orthographicSize * 1.65f * 250f) / 1920f, 0);
+        }
+        status = !status;
+    }
+}
---
+++ Assets/Scripts/Gameplay/ItemController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemController : MonoBehaviour
+{
+    private void OnMouseUp()
+    {
+        gameObject.SetActive(false);
+    }
+}
